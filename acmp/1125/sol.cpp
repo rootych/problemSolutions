8a13e55161bc8e2e91a9d88f731dd44c91732bc0
Manas SAMATOVICH <rootych@gmail.com>
2018-11-08 15:49:16 +0600

    new code again

---
+++ acmp/1125/sol.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+ 
+int main(){
+ 
+    int n, i = 1;
+    cin >> n;
+    while((i * i) <= n){
+        cout << i * i << " ";
+        i++;
+    }
+ 
+    return 0;
+}
